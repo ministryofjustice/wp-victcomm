f6d87e78e0b24688235dea72421369a4e1f14467
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-06-02 09:41:11 +0100

    Added embedded survey code to Current reviews page

--- wp-content/themes/victims-commissioner/templates/content-page.php
+++ wp-content/themes/victims-commissioner/templates/content-page.php
@@ -1,2 +1,10 @@
 <?php the_content(); ?>
+
+<?php
+	if ( is_page( 'current-review' ) ) {    
+    	echo '<script type="text/javascript" src="http://survey.euro.confirmit.com/wix/inline.aspx?p=p1843964285&v=2010&qt=false&xhr=false&__ckey=D29FC91F39B56F8E1C96266CE55E7FD3"></script>';
+	} else { 
+    	echo '';
+	}	
+?>
 <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
