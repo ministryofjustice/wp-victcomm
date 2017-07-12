86ef762f08c23f82834d77cc8cabf86105ef4bc6
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-07-12 14:15:17 +0100

    Move project to roots/bedrock directory structure.

--- wp-content/themes/victims-commissioner/lib/image-sizes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Homepage images
-add_image_size('homepage-lg', '555', '300', array('middle','middle'));
-add_image_size('homepage-md', '455', '250', array('middle','middle'));
-add_image_size('homepage-sm', '345', '200', array('middle','middle'));
-add_image_size('homepage-xs', '705', '400', array('middle','middle'));
