0d9480533cdb0a86623cffc3794f69917d761f3e
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-07-18 17:00:56 +0100

    Remove hardcoded Google Analytics tracking in favour or WordPress plugin.

--- web/app/themes/victims-commissioner/templates/head.php
+++ web/app/themes/victims-commissioner/templates/head.php
@@ -10,19 +10,6 @@
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 
-<!-- Google Analytics start -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-49127354-4', 'auto');
-  ga('send', 'pageview');
-
-</script>
-<!-- Google Analytics end -->
-
 <!--[if lt IE 9]>
 <link rel="stylesheet" type="text/css" href="wp-content/themes/victims-commissioner/assets/css/old-ie.css">
 <script src="wp-content/themes/victims-commissioner/assets/js/html5shiv.min.js"></script>
