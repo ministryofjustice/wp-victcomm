b2d4d9f62d74f483b8b355cbc6626e12df5945b7
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-07-18 16:57:10 +0100

    Update OptionTree plugin.
    - Move plugin out of theme and require as a composer dependency.
    - Upgrade to the latest version to avoid PHP notices being thrown for use of deprecated method.

--- web/app/themes/victims-commissioner/lib/extras.php
+++ web/app/themes/victims-commissioner/lib/extras.php
@@ -22,15 +22,10 @@ function roots_wp_title($title) {
 add_filter('wp_title', 'roots_wp_title', 10);
 
 /* Setup option tree */
-add_filter('ot_theme_mode', '__return_true');
 add_filter('ot_show_pages', '__return_false');
 add_filter('ot_show_new_layout', '__return_false');
 add_filter('ot_use_theme_options', '__return_true');
-
 add_filter('ot_header_version_text', '__return_null');
-
-//load_template( trailingslashit( get_template_directory() ) . 'inc/theme-options.php' );
-require_once (trailingslashit(get_template_directory()) . 'option-tree/ot-loader.php');
 
 /**
  * Meta Boxes
